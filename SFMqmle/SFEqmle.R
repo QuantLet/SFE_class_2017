91fae6a5e17303cabcc1a50346d82de19694f1b2
paulinakurowska <kurowskapaulina@gmail.com>
2018-02-04 23:01:13 +0100

    new update

--- SFMqmle/SFEqmle.R
+++ SFMqmle/SFEqmle.R
@@ -1,13 +1,24 @@
 # clear variables and close windows
 rm(list = ls(all = TRUE))
 graphics.off()
-setwd("C:/Users/ThinkPad/Desktop/TeXBeamer")
+
 # install and load packages
 if (!require("fGarch")) {
-  install.packages("fGarch")
+    install.packages("fGarch")
 }
 library(fGarch)
-
+if (!require("grid")) {
+    install.packages("grid")
+}
+library(grid)
+if (!require("gridExtra")) {
+    install.packages("gridExtra")
+}
+library(gridExtra)
+if (!require("gridGraphics")) {
+  install.packages("gridGraphics")
+}
+library(gridGraphics)
 
 #set seed
 set.seed(134)
@@ -17,8 +28,6 @@ sim1000 = garchSim(spec = garchSpec(model = list(alpha = 0.9, beta = 0)),1000)
 #plot PACF of squared Observations
 sim10002 = sim1000^2
 pacf(sim10002)
-
-
 #ARCH model estimation
 model1000  =  garchFit(~garch(1), data =sim1000,cond.dist = "QMLE")
 #plot of Time Series
